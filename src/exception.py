b6ad32fa82146456db4e65295303186a6981c97b
sadavaidya <svaidya2320@gmail.com>
2025-01-17 12:23:21 +0000

    Completed Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -11,7 +11,6 @@ def error_message_detail(error, error_detail:sys):
     return error_message
 
 
-
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
@@ -20,5 +19,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-
-
